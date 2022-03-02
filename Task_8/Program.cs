9e4a3e25a53e7be656689c4545ed01dc6f71ced7
rillout <fedkv@gmail.com>
2022-03-02 22:19:03 +0300

    Add Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿//ДЗ Задача 8. На вход число N, на выход все четные числа от 1 до N
+
+Console.Write("Введите число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if(i % 2 == 0) Console.Write(i + ",");
+}
