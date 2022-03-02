b733093f35db23a84aba8b29cedb735b3328fc83
rillout <fedkv@gmail.com>
2022-03-02 22:16:14 +0300

    Add Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,6 @@
+﻿//ДЗ Задача 6. На вход число, на выход - является ли оно четным
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine("Число " + number + " четное");
+else Console.WriteLine("Число " + number + " нечетное");
